15889de620a3b3856484f7dd924573243339fccb
Xurui Chen <xc1454@ip-10-10-1-15.ec2.internal>
2018-09-26 10:49:22 -0400

    updata homework_3 and homework_4

--- HW3_xc1454/get_bus_info_xc1454.py
+++ HW3_xc1454/get_bus_info_xc1454.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import pylab as pl
 import os
 import json
@@ -13,7 +14,7 @@ bus_line = 'MTA%20NYCT_' + sys.argv[2]
 
 fout = open(sys.argv[3], "w")
 
-url = "http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=%s&LineRef=%s"%(apikey, bus_line)
+url = "http://api.prod.obanyc.com/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s"%(apikey, bus_line)
 
 # get data through api
 response = urllib.urlopen(url)
@@ -24,12 +25,14 @@ fout.write("Latitude,Longitude,Stop Name,Stop Status" + "\n")
 for item in data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']:
     Longitude = item['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
     Latitude = item['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
-    Stop_Name = item['MonitoredVehicleJourney']['MonitoredCall']['StopPointName']
-    if Stop_Name == None:
-        Stop_Name == "N/A"
-    Stop_Status = item['MonitoredVehicleJourney']['MonitoredCall']['Extensions']['Distances']['PresentableDistance']
-    if Stop_Status == None:
-        Stop_Status == "N/A"
+    if item['MonitoredVehicleJourney']['OnwardCalls'] == {}:
+        Stop_Name = 'N/A'
+        Stop_Status = 'N/A'
+    else:
+        Stop_Name = item['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
+        Stop_Status = item['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
+
+
     #print(Stop_Name, Stop_Status)
     fout.write(str(Latitude) + "," + str(Longitude) + "," + Stop_Name + "," + Stop_Status + "\n")
 
--- HW3_xc1454/show_bus_locations_xc1454.py
+++ HW3_xc1454/show_bus_locations_xc1454.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import pylab as pl
 import os
 import json
